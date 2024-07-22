5257051116491e6d1c446fe0d307b8523e130786
himank147 <himank.147@gmail.com>
2024-07-22 21:11:54 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,16 @@
+import random
+import string
+def main():
+    
+    print("Welcome to our Random Password Generator")
+    length=int(input("Enter the length of password you want: "))
+    lowerD=string.ascii_lowercase
+    upperD=string.ascii_uppercase
+    digitD=string.digits
+    symbolsD=string.punctuation
+    combine=lowerD+upperD+digitD+symbolsD
+    x=random.sample(combine,length)
+    password="".join(x)
+    print(password)
+    main()
+main()
